fdcab8b49d2e7e836882407953b3d46776eb1154
Hoang Truong <truonghuynhduchoang@gmail.com>
2024-01-30 15:54:04 +0700

    feat: implememt fitlife-service application

---
+++ fitlife-app/fitlife-auth/src/main/java/com/fitlife/fitlifeauth/FitlifeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.fitlife.fitlifeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlifeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitlifeAuthApplication.class, args);
+    }
+
+}
---
+++ fitlife-app/fitlife-auth/src/test/java/com/fitlife/fitlifeauth/FitlifeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitlife.fitlifeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitlifeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
